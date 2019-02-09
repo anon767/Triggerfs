4026ee0371fd19377b8c76838ad986c7e98070ca
tom <git@thecout.com>
2019-02-09 22:58:12 +0100

    test

--- filesystem/CFS.go
+++ filesystem/CFS.go
@@ -1,7 +1,6 @@
 package filesystem
 
 import (
-	"fmt"
 	"github.com/hanwen/go-fuse/fuse"
 	"github.com/hanwen/go-fuse/fuse/nodefs"
 	"github.com/hanwen/go-fuse/fuse/pathfs"
@@ -12,21 +11,16 @@ Configurable File System (CFS)
 
 The Idea?
 
-act as Loopback FS and eventually call a hooked method depending on maaybe config file?
+act as Loopback FS and eventually call a hooked method depending on maybe config file?
 */
 
 type CFS struct {
 	pathfs.FileSystem
 }
 
-func (me *CFS) OpenDir(name string, context *fuse.Context) (c []fuse.DirEntry, code fuse.Status) {
-
-	fmt.Print("Here can I call Stuff")
-
-	return me.FileSystem.OpenDir(name, context)
-}
-
 func (me *CFS) Open(name string, flags uint32, context *fuse.Context) (file nodefs.File, code fuse.Status) {
-	fmt.Print("Here can I call Stuff")
-	return me.FileSystem.Open(name, flags, context)
+
+	return nodefs.NewDataFile([]byte("blubbediblubb")), fuse.OK
+	//base case
+	//return me.FileSystem.Open(name, flags, context)
 }
